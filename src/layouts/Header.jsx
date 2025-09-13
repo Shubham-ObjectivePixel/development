86b25d1a759fd6633194cdfc5e194def1e28c3cd
Shubham Soni <shubham@objectivepixel.in>
2025-09-13 13:20:27 +0530

    Update 1.6

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
             <header className="p-4 fixed w-full top-0 backdrop-blur-[12px] z-10 ">
                 <div className="container">
                     <div className="flex flex-wrap flex-row justify-between items-center">
-                        <a href="/">
+                        <a href="/" className="w-[30%]">
                             <img src="/src/assets/images/logo.png" width={'175px'} alt="" />
                         </a>
                         <Navigation></Navigation>
-                        <h6 className='text-white text-xl'>Think Beyond With. <span className='text-op-green font-medium'>Shubham.</span></h6>
+                        <h6 className='text-white text-xl w-[30%] text-right'>Think Beyond With. <span className='text-op-green font-medium'>Shubham.</span></h6>
                     </div>
                 </div>
             </header>
