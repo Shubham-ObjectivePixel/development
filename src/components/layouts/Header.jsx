d2bc0b8d29f9d650fdef05d5d6b41b150e3816a1
Shubham Soni <shubham@objectivepixel.in>
2025-09-22 13:18:30 +0530

    More updates

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,10 +1,11 @@
 import Logo from "../ui/Logo"
 import Navigation from "../layouts/Navigation"
 import Tagline from "../ui/Tagline"
+
 const Header = () => {
     return (
         <>
-            <header className="p-4 fixed w-full top-0 backdrop-blur-[12px] z-20 ">
+            <header className="p-4 fixed w-full top-0 backdrop-blur-[12px] z-20">
                 <div className="container">
                     <div className="flex flex-wrap flex-row justify-between items-center">
                         <Logo></Logo>
