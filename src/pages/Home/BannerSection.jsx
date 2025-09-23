fbbacf15e28c9c447b38f083ebd787d536d992b0
Shubham Soni <shubham@objectivepixel.in>
2025-09-23 10:35:43 +0530

    Update

--- src/pages/Home/BannerSection.jsx
+++ src/pages/Home/BannerSection.jsx
@@ -11,10 +11,7 @@ const BannerSection = () => {
 
     return (
         <>
-            <section
-                id="op-banner-section"
-                className="relative h-screen overflow-hidden px-8"
-            >
+            <section id="op-banner-section" className="relative h-screen overflow-hidden px-8">
                 <div className="container mx-auto h-full relative">
                     <div className="absolute top-0 left-0 w-1/2 h-full flex items-center justify-end z-10">
                         <h1 className="text-white text-[72px] leading-[108px] font-extrabold text-right heading-motion">
@@ -22,7 +19,6 @@ const BannerSection = () => {
                             your <span className="text-op-green">vision</span> into life.
                         </h1>
                     </div>
-
                     <div className="flex items-center justify-end h-full">
                         <div className="w-2/3 h-[70vh]">
                             <div className="bg-black w-full h-full rounded-xl this-animate"></div>
@@ -30,8 +26,6 @@ const BannerSection = () => {
                     </div>
                 </div>
             </section>
-
-
         </>
     )
 }
