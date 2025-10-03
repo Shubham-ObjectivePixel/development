b2e5578634e5aba0b7e2801d2d23ea07a8c807ec
Shubham <shubham@objectivepixel.in>
2025-10-03 09:25:59 +0530

    demo

--- src/components/layouts/Navigation.jsx
+++ src/components/layouts/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
             <div className="w-[33%] flex flex-wrap justify-end relative">
                 <div className="w-[60%] text-white border-b border-op-white-25 py-3 px-4 navigation-section cursor-pointer">
                     <div className="flex flex-wrap justify-between items-center">
-                        <span className="uppercase text-sm tracking-widest">Vision</span>
+                        <span className="uppercase text-sm tracking-widest">Vision?</span>
                         <TbGridDots className="w-[25px] h-[25px]" />
                     </div>
                 </div>
