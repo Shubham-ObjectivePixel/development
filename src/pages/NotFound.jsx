d3ae279f041bcbaeec741050666646d73ad06b85
Shubham Soni <shubham@objectivepixel.in>
2025-10-06 16:23:18 +0530

    404 Page Update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,10 +6,10 @@ const NotFoundPage = () => {
             <main className="h-screen w-screen">
                 <GlowingLight />
                 <div className="flex flex-wrap h-full w-full items-center justify-center relative z-10">
-                    <div className="text-center">
-                        <h1 className="mb-4 text-4xl font-makizlo-font">404</h1>
-                        <p className="mb-4 text-xl">Oops! Page not found</p>
-                        <a href="/" className="">Return to Home</a>
+                    <div className="text-center text-white">
+                        <h1 className="text-[7vw] font-makizlo-font">404</h1>
+                        <p className="text-xl">Oops! Page not found</p>
+                        <a href="/" className="hover:text-op-green">Return to Home</a>
                     </div>
                 </div>
             </main>
