a579c0c5d508b1a6d66e4f3341220f61a07d18f3
Shubham Soni <shubham@objectivepixel.in>
2025-10-06 12:42:50 +0530

    404 Page update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,17 @@
+import GlowingLight from "../components/ui/GlowingLight";
+
 const NotFoundPage = () => {
     return (
         <>
-            <main>
-                <h1>404 page</h1>
+            <main className="h-screen w-screen">
+                <GlowingLight />
+                <div className="flex flex-wrap h-full w-full items-center justify-center relative z-10">
+                    <div className="text-center">
+                        <h1 className="mb-4 text-4xl font-makizlo-font">404</h1>
+                        <p className="mb-4 text-xl">Oops! Page not found</p>
+                        <a href="/" className="">Return to Home</a>
+                    </div>
+                </div>
             </main>
         </>
     );
