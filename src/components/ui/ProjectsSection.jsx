399d909e9027510252d8eb1e91dcf514e34d7d39
Shubham Soni <shubham@objectivepixel.in>
2025-10-07 19:25:52 +0530

    Update

--- src/components/ui/ProjectsSection.jsx
+++ src/components/ui/ProjectsSection.jsx
@@ -4,11 +4,11 @@ const ProjectsSection = () => {
     return (
         <>
             <section className="h-[200vh] relative rounded-xl m-8 p-8">
-                <h2 className="section-title-effect uppercase text-[10vw] text-center leading-normal text-op-white-25 fixed top-50">
+                <h2 className="section-title-effect uppercase text-[10vw] text-center leading-normal text-op-white-25 fixed">
                     {/* Featured Work */}
                     Lorem Ipsum
                 </h2>
-                <div className="relative z-10">
+                <div className="relative z-10 pt-[8.5vw]">
                     <div className="flex justify-between gap-3">
                         <Cards />
                         <Cards />
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,9 @@
 import Header from "../components/layouts/Header"
 import Footer from "../components/layouts/Footer"
 
-// import BannerSection from "../components/ui/BannerSection";
+import GlowingLight from "../components/ui/GlowingLight";
+import BannerSection from "../components/ui/BannerSection";
 import ProjectsSection from "../components/ui/ProjectsSection";
-// import GlowingLight from "../components/ui/GlowingLight";
 
 const HomePage = () => {
     return (
