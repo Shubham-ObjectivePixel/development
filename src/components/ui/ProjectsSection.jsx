2457dc3c3b8d9938022a7fe8c6fec752e742e4e2
Shubham Soni <shubham@objectivepixel.in>
2025-10-08 18:52:00 +0530

    Final Update

--- src/components/ui/ProjectsSection.jsx
+++ src/components/ui/ProjectsSection.jsx
@@ -11,18 +11,18 @@ const ProjectsSection = () => {
 
         const observer = new IntersectionObserver(
             ([entry]) => {
-                // Add class when the top of the section touches the top of the viewport
+                
                 if (entry.isIntersecting && entry.boundingClientRect.top <= 0) {
-                    setActiveSection(sectionId); // Set the active section when the top is at the top
+                    setActiveSection(sectionId);
                 } 
-                // Remove class when the bottom of the section touches the top of the viewport
-                else if (!entry.isIntersecting && entry.boundingClientRect.bottom <= 0) {
-                    setActiveSection(null); // Remove the active section when the bottom is at the top
+                
+                else if (!entry.isIntersecting && entry.boundingClientRect.bottom <= 0 || entry.boundingClientRect.top >= 0) {
+                    setActiveSection(null);
                 }
             },
             {
-                rootMargin: "0px 0px -100% 0px", // When the bottom of the section touches the viewport top
-                threshold: [0, 1], // Trigger at both 0% and 100% visibility
+                rootMargin: "0px 0px -100% 0px",
+                threshold: [0, 1],
             }
         );
 
@@ -35,11 +35,11 @@ const ProjectsSection = () => {
     return (
         <section
             ref={sectionRef}
-            id={sectionId}  // Unique ID for this section
+            id={sectionId}
             data-bg-type="white"
-            className="h-[200vh] relative z-10 rounded-xl bg-black"
+            className="h-[200vh] relative z-10 rounded-xl bg-white"
         >
-            <h2 className="section-title-effect uppercase text-[10vw] text-center leading-normal">
+            <h2 className="section-title-effect text-white uppercase text-[10vw] text-center leading-normal">
                 Work
             </h2>
             <div className="relative z-10 pt-[8.5vw]">
