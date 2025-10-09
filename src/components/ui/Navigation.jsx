414be5f9fdf1dd3c611a55cf3765a2580b9f3767
Shubham Soni <shubham@objectivepixel.in>
2025-10-09 14:51:08 +0530

    update

--- src/components/ui/Navigation.jsx
+++ src/components/ui/Navigation.jsx
@@ -36,16 +36,13 @@ const Navigation = () => {
                 <div className="navigation-wrapper invisible fixed top-0 left-0 w-screen h-screen">
                     <div className="navigation-animation bg-op-navyBlue w-[80px] h-[80px] rounded-[50%] fixed right-0 top-0 scale-[0]"></div>
                     <div className="navigation-content relative z-10 w-full h-full">
-                        <div className="absolute md:top-6 top-4 md:left-6 left-4">
-                            <img src="https://www.mwfloridalaw.com/wp-content/uploads/2019/12/logo.png" alt="Logo" />
-                        </div>
                         <div onClick={navigationHandlerClose} className="op-rotation-buttons navigation-close-button absolute md:top-6 top-4 md:right-6 right-4 sm:w-[130px] w-[80px] sm:h-[130px] h-[80px] rounded-full z-10 flex flex-wrap items-center justify-center cursor-pointer text-op-darkBlue bg-op-green hover:bg-op-white-25 hover:text-white">
                             <span className="uppercase text-sm tracking-widest">close</span>
                         </div>
                         <div className="navigation-socialMedia absolute bottom-6 right-6">
                             <a href="#" target="_blank" className="uppercase text-sm tracking-widest text-white">Instagram</a>
                         </div>
-                        <div className="main-navigation h-screen w-[89%] pl-[8vw] pt-[100px] invisible">
+                        <div className="main-navigation h-screen w-[89%] pl-[8vw] invisible">
                             <nav className="flex flex-col h-full justify-center">
                                 <ul className="flex flex-col lg:gap-[4vw] md:gap-[7vw] gap-[10vw] justify-between">
                                     <li className="op-nav-item"><a href="/about" className="text-stroke font-black uppercase text-op-white-25 hover:text-white font-makizlo-font lg:text-[8vw] md:text-[10vw] text-[14vw] leading-[normal] tracking-widest transition duration-700 ease-in-out">About</a></li>
