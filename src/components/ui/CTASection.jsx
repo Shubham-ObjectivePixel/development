5128cfbbe407ba28a9b904b282d0757c04002641
Shubham Soni <shubham@objectivepixel.in>
2025-10-13 19:13:27 +0530

    More updates

--- src/components/ui/CTASection.jsx
+++ src/components/ui/CTASection.jsx
@@ -15,9 +15,9 @@ const CTASection = () => {
     }, []);
 
     return (
-        <section ref={el} className="op-sec-scale-zoom xl:px-24 lg:px-20 md:px-16 px-8 py-24 bg-black relative rounded-xl m-8 scale-[1] origin-top">
+        <section ref={el} className="op-sec-scale-zoom xl:px-24 lg:px-20 md:px-16 px-8 py-24 bg-secondaryColor relative rounded-xl m-8 scale-[1] origin-top">
             <div className="flex flex-wrap items-center justify-center md:gap-16 gap-8">
-                <h2 className="section-title-animation text-secondaryColor text-[8vw] text-center leading-[normal] opacity-[0.01]">No magic formula just Consistent effort.</h2>
+                <h2 className="section-title-animation text-primaryColor text-[8vw] text-center leading-[normal] opacity-[0.01]">No magic formula just Consistent effort.</h2>
                 <a href="" className="inline-block rounded-full px-5 py-2 transition duration-500 transition-discrete bg-gradientColor text-black font-medium leading-none">About Me</a>
             </div>
         </section>
